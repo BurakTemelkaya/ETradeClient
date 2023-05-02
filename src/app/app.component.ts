164eb44e65626690504fa674b18dd971f496eb8b
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-05-02 04:41:51 +0300

    Lesson 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $:any
 
 @Component({
@@ -7,9 +9,23 @@ declare var $:any
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private toastrService:CustomToastrService){
+    toastrService.message("Merhaba","Burak",{
+      messageType:ToastrMessageType.Info,
+      position:ToastrPosition.TopCenter
+    });
+    toastrService.message("Merhaba","Burak",{
+      messageType:ToastrMessageType.Success,
+      position:ToastrPosition.BottomCenter
+    });
+    toastrService.message("Merhaba","Burak",{
+      messageType:ToastrMessageType.Error,
+      position:ToastrPosition.TopLeft
+    });
+    toastrService.message("Merhaba","Burak",{
+      messageType:ToastrMessageType.Warning,
+      position:ToastrPosition.BottomRight
+    });
+  }
   title = 'ETradeClient';
 }
-
-// $(document).ready(()=>{
-//   alert("test");
-// })
