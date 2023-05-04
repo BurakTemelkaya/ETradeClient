7e76c880fd7dee0d5644f3de31c9906c83d24f19
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-05-04 05:15:35 +0300

    Lesson 20

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -23,6 +23,26 @@ export class CreateComponent extends BaseComponent {
     create_product.price = parseFloat(price.value);
     create_product.stock = parseInt(stock.value);
 
+    if (!name.value) {
+      this.alertify.message("Lütfen ürün adını giriniz.",
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        })
+      return;
+    }
+
+    if (parseInt(stock.value) <= 0) {
+      this.alertify.message("Lütfen stok bilgisini doğru giriniz.",
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        })
+      return;
+    }
+
     this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.ballScaleMultiple);
       this.alertify.message("Ürün başarıyla eklenmiştir", {
@@ -30,6 +50,13 @@ export class CreateComponent extends BaseComponent {
         messageType: MessageType.Success,
         position: Position.TopRight
       });
-    });    
+    }, errorMessage => {
+      this.alertify.message(errorMessage,
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        })
+    });
   }
 }
